68b4c9d6140222b17a2e607c9472619a5265a6e7
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-01 00:17:51 +0300

    Simplified code. (Now using Mui AppBar).

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,19 @@
-import { makeStyles } from '@material-ui/core';
+import { AppBar, Toolbar } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 import NavBarButtons from './NavBarButtons';
 import NavBarNameTitle from './NavBarNameTitle';
 
-const useStyles = makeStyles({
-  root: {
-    borderStyle: 'solid',
-    borderWidth: '0.5px',
-    //borderColor: 'green',
-    borderColor: 'transparent',
-
-    position: 'fixed',
-    width: '100%',
-    background: 'inherit',
-    textAlign: 'center',
-  },
-});
-
 export default function NavBar({ onClick }) {
-  const classes = useStyles();
-
   return (
-    <header className={classes.root}>
-      <NavBarNameTitle />
-      <NavBarButtons onClick={onClick} />
-    </header>
+    <AppBar style={{ position: 'fixed', alignItems: 'center' }} color='inherit' position='absolute'>
+      <Toolbar>
+        <NavBarNameTitle />
+      </Toolbar>
+      <Toolbar>
+        <NavBarButtons onClick={onClick} />
+      </Toolbar>
+    </AppBar>
   );
 }
 
