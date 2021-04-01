2d12ac78a5c0cc9deccf57a8955f8995c62dae1f
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-01 15:54:53 +0300

    highlighted come words.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,13 +4,19 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import BeenhereIcon from '@material-ui/icons/Beenhere';
 import ListIconTextItem from './ListIconTextItem';
 
+import ItalicGreen from './ItalicGreen';
+
 const SkillsConfig = [
   {
     language: 'C++',
     level: 'skillful',
     about: [
-      'When I started learning programming in high school, the first language was C.',
-      'Once I developed my algorithmic thinking, it was pretty easy for me to switch to C++.',
+      <>
+        When I started learning programming in high school, the first language was <ItalicGreen>C</ItalicGreen>.
+      </>,
+      <>
+        Once I developed my algorithmic thinking, it was pretty easy for me to switch to <ItalicGreen>C++</ItalicGreen>.
+      </>,
       'I like the fact that it is very efficient and that the programmer has a lot of control over what happens to the memory.',
       'This is the language I used in programming contests.',
     ],
@@ -19,10 +25,21 @@ const SkillsConfig = [
     language: 'Java',
     level: 'beginner',
     about: [
-      "Java was the second programming language I've learned.",
-      'In high school I started playing Minecraft Multiplayer. This game was made in  Java (surprise).',
-      'Over time, I became more and more curious about what was behind the game.',
-      "After a bit of research I started learning how can I modify the gameplay via Spigot API, a minecraft's server API.",
+      <>
+        <ItalicGreen>Java</ItalicGreen> was the second programming language I&apos;ve learned.
+      </>,
+      <>
+        In high school I started playing <ItalicGreen>Minecraft Multiplayer</ItalicGreen>. This game was made in{' '}
+        <ItalicGreen>Java</ItalicGreen>
+        (surprise).
+      </>,
+      <>
+        Over time, I became more and more curious about what was <ItalicGreen>behind the game</ItalicGreen>.
+      </>,
+      <>
+        After a bit of research I started learning how can I modify the gameplay via{' '}
+        <ItalicGreen>Spigot API</ItalicGreen>, a minecraft&apos;s server API.
+      </>,
       'It was a lot of fun learning some Java through gaming.',
     ],
   },
@@ -31,17 +48,33 @@ const SkillsConfig = [
     level: 'beginner',
     about: [
       'In my first year of college I became more and more interested in how games are created.',
-      'When I started this journey, I had to choose between { C++ & Unreal Engine } and { C# & Unity Engine } and since I already knew C++, I chose the C# version because I wanted something new.',
-      'I spent a summer learning the features of C# and Unity Engine. It was fun to come up with ideas and watch them come to life.',
-      "At this level I see C# like a combination of C++ & Java but I'm sure it's much more than this.",
+      <>
+        When I started this journey, I had to choose between{' '}
+        <ItalicGreen>&#123; C++ & Unreal Engine &#125;</ItalicGreen> and{' '}
+        <ItalicGreen>&#123; C# & Unity Engine &#125;</ItalicGreen> and since I already knew{' '}
+        <ItalicGreen>C++</ItalicGreen>, I chose the <ItalicGreen>C#</ItalicGreen> version because I wanted{' '}
+        <ItalicGreen>something new</ItalicGreen>.
+      </>,
+      <>
+        I spent a summer learning the features of <ItalicGreen>C# & Unreal Engine</ItalicGreen>. It was fun to come up
+        with ideas and watch them come to life.
+      </>,
     ],
   },
   {
     language: 'JavaScript & ReactJS',
     level: 'beginner',
     about: [
-      'As a fun fact, the first time I used JavasSript was in 10th grade in high school for a grid test for any web page. Then I hoped it would be the first and last time I use it.',
-      'The real adventure with JavaScript started in the second year of college when I enrolled in the "Tap that job" project and was admitted to the JavaScript & ReactJS courses at Ubisoft.',
+      <>
+        As a <ItalicGreen>fun fact</ItalicGreen>, the first time I used <ItalicGreen>JavaSript</ItalicGreen> was in 10th
+        grade in high school for a <ItalicGreen>grid test</ItalicGreen> for a web page. Then I hoped it would be the{' '}
+        <ItalicGreen>first and last time</ItalicGreen> I use it.
+      </>,
+      <>
+        The real adventure with <ItalicGreen>JavaScript</ItalicGreen> started in the second year of college when I
+        enrolled in the <ItalicGreen>&quot;Tap that job&quot;</ItalicGreen> project and was admitted to the{' '}
+        <ItalicGreen>JavaScript & ReactJS</ItalicGreen> courses at Ubisoft.
+      </>,
       'Now it seems a much friendlier language than when we first met.',
     ],
   },
