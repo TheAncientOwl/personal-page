bad8e47b9e1cbae0e42c8d842839c471505a6220
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-01 11:42:48 +0300

    made 'projects'.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,79 @@
-import React from 'react';
+import {
+  Link,
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+  Box,
+  Divider,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  inline: {
+    display: 'inline',
+  },
+}));
 
 export default function Projects() {
+  const userName = 'TheAncientOwl';
+  const [repos, setRepos] = useState([]);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/' + userName + '/repos?sort=created')
+      .then(response => response.json())
+      .then(data => setRepos(data));
+  }, [userName]);
+
+  const listItems = repos.map((repo, index) => (
+    <>
+      <ListItem key={index} alignItems='center'>
+        <ListItemAvatar>
+          <Avatar alt={repo.owner.login} src={repo.owner.avatar_url} />
+        </ListItemAvatar>
+
+        <ListItemText
+          primary={
+            <>
+              <Link
+                style={{ fontStyle: 'italic', color: 'green', fontWeight: 'bold' }}
+                href={repo.html_url}
+                target='_blank'>
+                {repo.name}
+              </Link>
+              <Typography style={{ fontStyle: 'italic', color: 'green' }} variant='body2' color='textPrimary'>
+                &lt; {repo.language} &gt;
+              </Typography>
+            </>
+          }
+          secondary={
+            <Typography variant='body2' className={classes.inline} color='textPrimary'>
+              {repo.owner.login}
+              {' — ' + repo.description}
+            </Typography>
+          }
+        />
+      </ListItem>
+      <Divider style={{ background: 'gray' }} />
+    </>
+  ));
+
   return (
-    <div>
-      Projects Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-      laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-      laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-      laborum.
-    </div>
+    <>
+      <Typography variant='h5' align='center'>
+        GitHub Repos
+      </Typography>
+      <Box display='flex' alignItems='center' justifyContent='center'>
+        <List className={classes.root}>{listItems}</List>
+      </Box>
+    </>
   );
 }
