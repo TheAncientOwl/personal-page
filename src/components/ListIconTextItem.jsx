9deafa07ccc6568ad8d9bde314e68e9bdcd40ceb
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2021-04-01 12:43:33 +0300

    set color primary/secondary.

--- src/components/ListIconTextItem.jsx
+++ src/components/ListIconTextItem.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ListItemText, ListItem, ListItemIcon, Divider } from '@material-ui/core';
+import { ListItemText, ListItem, ListItemIcon, Divider, Typography } from '@material-ui/core';
 
 export default function ListIconTextItem({ icon, prop1, prop2 }) {
   return (
     <>
       <ListItem>
-        <ListItemIcon style={{ color: 'gray' }}>{icon}</ListItemIcon>
+        <ListItemIcon color='secondary'>{icon}</ListItemIcon>
         <ListItemText>
-          <span style={{ fontStyle: 'italic' }}>{prop1}</span> &gt; <span style={{ color: 'green' }}>{prop2}</span>
+          <i>{prop1}</i> &gt;{' '}
+          <Typography component='span' color='primary'>
+            {prop2}
+          </Typography>
         </ListItemText>
       </ListItem>
       <Divider />
